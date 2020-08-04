0ef61ef28d90f974aa1238577421f0a70cd8602f
tomklecha <tom.klecha.dev@gmail.com>
2020-08-04 10:19:38 +0100

    added JetPack Navigation

--- app/src/main/java/com/tkdev/nuomaddressbook/MainActivity.kt
+++ app/src/main/java/com/tkdev/nuomaddressbook/MainActivity.kt
@@ -2,14 +2,24 @@ package com.tkdev.nuomaddressbook
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.replace
-import com.tkdev.nuomaddressbook.views.ContactsFragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction().replace(R.id.activity_main, ContactsFragment()).commit()
+        val navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.contactsFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
